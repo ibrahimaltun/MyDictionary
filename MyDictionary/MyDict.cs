08ffbdb002ed0a277d0f30bffc80c418390d7d1c
ibrahim <53177796+ibrahimaltun@users.noreply.github.com>
2021-01-29 10:30:03 +0300

    Add project files.

---
+++ MyDictionary/MyDict.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey,TValue>
+    {
+        TKey[] _keys;
+        TValue[] _values;
+
+        public MyDictionary()
+        {
+            _keys = new TKey[0];
+            _values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempkeys = _keys;
+            TValue[] tempValues = _values;
+
+            _keys = new TKey[_keys.Length + 1];
+            _values = new TValue[_values.Length + 1];
+
+            for (int i = 0; i <tempkeys.Length; i++)
+            {
+                _keys[i] = tempkeys[i];
+            }
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                _values[i] = tempValues[i];
+            }
+            _keys[_keys.Length - 1] = key;
+            _values[_values.Length - 1] = value;
+        }
+        public TKey[] Keys
+        {
+            get { return _keys; }
+        }
+
+        public TValue[] Values
+        {
+            get { return _values; }
+        }
+
+        public void List()
+        {
+            for (int i = 0; i < _keys.Length; i++)
+            {
+                Console.WriteLine(_keys[i] + ":" + _values[i]);
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> MyCars = new MyDictionary<string, int>();
+
+            MyCars.Add("BMW", 2020);
+            MyCars.Add("AUDİ", 2005);
+            MyCars.Add("MERCEDES", 2018);
+
+            Console.WriteLine("Car : " + MyCars.Keys[0] + "     Year of construction : " + MyCars.Values[0]);
+            Console.WriteLine("Car : " + MyCars.Keys[1] + "     Year of construction : " + MyCars.Values[1]);
+            Console.WriteLine("Car : " + MyCars.Keys[2] + "     Year of construction : " + MyCars.Values[2]);
+            Console.ReadLine();
+        }
+    }
+}
